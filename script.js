3680e79e953cf90b05b0b0d18092050154559e65
wahab95 <a.wahab1996@gmail.com>
2024-06-09 00:51:09 +0300

    chore: Add script.js to index.html for deferred loading

---
+++ script.js
@@ -0,0 +1,16 @@
+//      ::::::::Vars::::::::
+
+
+
+
+
+
+//      ::::::::Functions & Methods::::::::
+function makegrid(row,col){
+    for( let i = 0; i < row; i++ ){
+        for( let k = 0; k < col; k++ ){
+
+        }
+    }
+}
+//      ::::::::Actions::::::::
